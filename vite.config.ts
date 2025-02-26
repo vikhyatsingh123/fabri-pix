203a2e9e02ed9d872a81b7fcc026f540d2b75f6d
Vikhyat Singh <vikhyat.singh@314ecorp.com>
2025-02-26 18:26:03 +0530

    initiated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/main.tsx",
+      name: "FabriPix",
+      fileName: "fabri-pix",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "fabric"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          fabric: "fabric",
+        },
+      },
+    },
+  },
+});

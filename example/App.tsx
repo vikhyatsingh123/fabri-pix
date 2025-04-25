341da41b631d7ff9d94ca2ccd7682b880f2d2434
Vikhyat Singh <vikhyat.singh@314ecorp.com>
2025-04-25 12:24:47 +0530

    updated image

--- example/App.tsx
+++ example/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
 	return (
 		<div>
 			<h1 style={{ marginBottom: '0px' }}>FabriPix Image Editor</h1>
-			<ImageEditorWrapper imageUrl='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1rHjvktRKnlYH_KhsC0dw4sMizgnKR_0QiQ&s' />
+			<ImageEditorWrapper imageUrl='https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630' />
 		</div>
 	);
 }
